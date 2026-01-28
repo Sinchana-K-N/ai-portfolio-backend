4ddce6d937fbfd36ca701dfc8d355aab1823ecce
Sinchana K N <sinchanaknsmg@gmail.com>
2026-01-29 01:53:35 +0530

    updated the allowedOrigins.

--- src/main/java/com/sinchana/ai_portfolio_backend/config/CorsConfig.java
+++ src/main/java/com/sinchana/ai_portfolio_backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://sinchanakn-portfolio.netlify.app/")
+                .allowedOrigins("http://localhost:5173","https://sinchanakn-portfolio.netlify.app/")
                 .allowedMethods("POST");
     }
 }
